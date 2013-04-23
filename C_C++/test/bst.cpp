f88792781aa555d998a65f31063e7debb35eb393
rsadhu <rakeshsadhu@gmail.com>
2013-04-23 09:54:29 +0530

    adding more into bst

--- C_C++/test/bst.cpp
+++ C_C++/test/bst.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<queue>
+#include<stack>
 
 
 typedef struct _tree
@@ -11,11 +12,14 @@ typedef struct _tree
 
 typedef enum
 {
+  INORDERITER,
+  POSTORDERITER,
+  PREORDERITER,
   INORDER,
   POSTORDER,
   PREORDER,
   BFS,
-  EBFS
+  SPIRAL
 } Type;
 
 BST *
@@ -31,29 +35,29 @@ _new (int d)
 void
 create (BST ** root, int d)
 {
-    BST *tra = *root;
-    while (tra)
+  BST *tra = *root;
+  while (tra)
     {
-        if (d > tra->data)
-        {
-            if (tra->right)
-                tra = tra->right;
-            else
-            {
-                tra->right = _new (d);
-                break;
-            }
-        }
-        else if (d < tra->data)
-        {
-            if (tra->left)
-                tra = tra->left;
-            else
-            {
-                tra->left = _new (d);
-                break;
-            }
-        }
+      if (d > tra->data)
+	{
+	  if (tra->right)
+	    tra = tra->right;
+	  else
+	    {
+	      tra->right = _new (d);
+	      break;
+	    }
+	}
+      else if (d < tra->data)
+	{
+	  if (tra->left)
+	    tra = tra->left;
+	  else
+	    {
+	      tra->left = _new (d);
+	      break;
+	    }
+	}
     }
 }
 
@@ -81,6 +85,78 @@ createRec (BST * root, int d)
 }
 
 void
+inOrderIter (BST * tra)
+{
+  std::stack < BST * >st;
+  st.push (tra);
+
+  while (!st.empty ())
+    {
+      if (tra)
+	{
+	  st.push (tra);
+	  tra = tra->left;
+	}
+      else
+	{
+	  tra = st.top ();
+	  printf ("   %d   ", tra->data);
+	  st.pop ();
+	  tra = tra->right;
+	}
+    }
+}
+
+void
+postOrderIter (BST * tra)
+{
+  std::stack < BST * >st;
+  st.push (tra);
+  while(!st.empty() || tra)
+  {
+      if(tra)
+      {
+          printf(" %d ",tra->data);
+          tra = tra->left;
+          st.push(tra);
+      }
+      else
+      {
+          tra = st.top();
+          st.pop();
+          if(tra->left)
+          printf(" %d ",tra->left->data);
+          if(tra->right)
+          printf(" %d ",tra->right->data);
+      }
+
+  }
+
+}
+
+void
+preOrderIter (BST * tra)
+{
+  std::stack < BST * >st;
+  while (!st.empty () || tra )
+    {
+      if (tra)
+	{
+	  printf (" %d ", tra->data);
+	  if (tra->right);
+	  st.push (tra->right);
+	  tra = tra->left;
+	}
+      else
+	{
+	  tra = st.top ();
+	  st.pop ();
+	}
+    }
+
+}
+
+void
 inOrder (BST * tra)
 {
   if (tra)
@@ -105,33 +181,63 @@ postOrder (BST * tra)
 void
 preOrder (BST * tra)
 {
-  if (tra)
-    {
+  if (tra){
       printf (" %d ", tra->data);
       inOrder (tra->left);
       inOrder (tra->right);
-    }
+  }
 }
 
-void bfs(BST *root)
+void
+bfs (BST * root)
 {
-    std::queue<BST*>  q;
-    q.push(root);
-    while(!q.empty())
+  std::queue < BST * >q;
+  q.push (root);
+  while (!q.empty ())
     {
-        BST *tmp = q.front();
-        printf(" %d ",tmp->data);
-        if(tmp->left)
-            q.push(tmp->left);
-        if(tmp->right)
-            q.push(tmp->right);
-            q.pop();
+      BST *tmp = q.front ();
+      printf (" %d ", tmp->data);
+      if (tmp->left)
+	q.push (tmp->left);
+      if (tmp->right)
+	q.push (tmp->right);
+      q.pop ();
     }
 }
 
-void eBfs(BST *root)
+void
+spiral (BST * root)
 {
-//    std::queue<BST*> q;
+  std::stack < BST * >st1;
+  std::stack < BST * >st2;
+  st1.push (root);
+
+  while (!st1.empty () || !st2.empty ())
+    {
+      while (!st1.empty ())
+	{
+	  BST *t = st1.top ();
+	  st1.pop ();
+	  if (t)
+	    {
+	      printf ("   %d  ", t->data);
+	      st2.push (t->right);
+	      st2.push (t->left);
+	    }
+	}
+
+      while (!st2.empty ())
+	{
+	  BST *t = st2.top ();
+	  st2.pop ();
+	  if (t)
+	    {
+	      printf ("   %d  ", t->data);
+	      st1.push (t->left);
+	      st1.push (t->right);
+	    }
+	}
+    }
 }
 
 void
@@ -155,9 +261,21 @@ display (BST * root, Type t)
       printf ("\ndisplay ::BFS \n");
       bfs (root);
       break;
-    case EBFS:
+    case SPIRAL:
       printf ("\ndisplay ::EBFS \n");
-      eBfs (root);
+      spiral (root);
+      break;
+    case INORDERITER:
+      printf ("\n display :: INORDERITER\n");
+      inOrderIter (root);
+      break;
+    case PREORDERITER:
+      printf ("\n display :: INORDERITER\n");
+      preOrderIter (root);
+      break;
+    case POSTORDERITER:
+      printf ("\n display :: INORDERITER\n");
+      postOrderIter(root);
       break;
     default:
       printf ("\ndisplay :: default \n");
@@ -166,42 +284,43 @@ display (BST * root, Type t)
 }
 
 
-void dNode(BST **root,int d)
+void
+dNode (BST ** root, int d)
 {
   BST *tra = *root;
-  BST *tmp=NULL;
-  int i=1;
-  while(tra)
-  {
-       if(d  > tra->data )
-       {
-           tmp =  tra;
-           i=0;
-           if(tra->right)
-               tra =  tra->right;
-               
-       }
-       else if(d < tra->data)
-       {
-           tmp =  tra;
-           i=1;
-           if(tra->left)
-                tra = tra->left;
-       }
-       else
-       {
-           printf("\n %d == %d \n ",tra->data ,d);
-           if(!tra->left  && !tra->right)
-           {
-               free(tra);
-               tra= NULL;
-               if(i==1)
-                   tmp->left = NULL;
-               if(i==0)
-                   tmp->right =  NULL;
-           }
-       }
-  }
+  BST *tmp = NULL;
+  int i = 1;
+  while (tra)
+    {
+      if (d > tra->data)
+	{
+	  tmp = tra;
+	  i = 0;
+	  if (tra->right)
+	    tra = tra->right;
+
+	}
+      else if (d < tra->data)
+	{
+	  tmp = tra;
+	  i = 1;
+	  if (tra->left)
+	    tra = tra->left;
+	}
+      else
+	{
+	  printf ("\n %d == %d \n ", tra->data, d);
+	  if (!tra->left && !tra->right)
+	    {
+	      free (tra);
+	      tra = NULL;
+	      if (i == 1)
+		tmp->left = NULL;
+	      if (i == 0)
+		tmp->right = NULL;
+	    }
+	}
+    }
 }
 
 
@@ -223,10 +342,10 @@ main (void)
     // createRec(root,arr[i]);
     create (&root, arr[i]);
 
- display (root, INORDER);
+// display (root, INORDER);
 // display (root, BFS);
 // display (root, EBFS);
- dNode(&root,12);
+/* dNode(&root,12);
  display (root, INORDER);
  dNode(&root,18);
  display (root, INORDER);
@@ -234,5 +353,12 @@ main (void)
  display (root, INORDER);
  dNode(&root,50);
  display (root, INORDER);
+ */
+  //display(root,SPIRAL);
+// display(root,POSTORDER);
+  //display(root,INORDERITER);
+  display (root, PREORDER);
+  display (root, PREORDERITER);
+  display (root, POSTORDERITER);
   return 0;
 }
