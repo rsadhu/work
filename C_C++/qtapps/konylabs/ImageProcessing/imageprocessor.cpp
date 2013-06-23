834a61e0886e5be5c09a7cc96bd5423661ed8a6f
rsadhu <rakeshsadhu@hotmail.com>
2013-06-24 00:02:05 +0530

    adding more changes in konylabs

--- C_C++/qtapps/konylabs/ImageProcessing/imageprocessor.cpp
+++ C_C++/qtapps/konylabs/ImageProcessing/imageprocessor.cpp
@@ -1,8 +1,36 @@
 #include "imageprocessor.h"
 
+MyImageList & MyImageList::operator <<(MyImage *img)
+{
+    if(!mHead)
+    {
+       mHead = img;
+    }else{
+
+    MyImage *tra;
+    for(tra= mHead;tra->next!=NULL;tra=tra->next);
+    tra->next = img;
+    }
+    return *this;
+}
+
+void MyImageList::ascending(CType t)
+{
+
+}
+
+void MyImageList::descending(CType t)
+{
+
+}
+
 ImageProcessor::ImageProcessor(QString &path)
 {
     mSrc = new QImage(path);
+    if(mSrc->isNull()==true)
+    {
+        qDebug()<<" file cudnt be opened\n";
+    }
 }
 
 ImageProcessor::~ImageProcessor()
@@ -14,25 +42,37 @@ ImageProcessor::~ImageProcessor()
 
 void ImageProcessor::process()
 {
-    for(int i=0;i<mSrc->width();i+=2){
+    for(int i=0;i<mSrc->width();i+=2)
+    {
         for(int j=0;j<mSrc->height();j+=2)
         {
-            QImage imgs = mSrc->copy(i,j,2,2);
-            mImages.push_back(imgs);
+            QImage img = mSrc->copy(i,j,2,2);
+
+            if ( false == img.isNull() )
+            {
+                int rv=0,gv=0,bv=0;
+                QVector<QRgb> v = img.colorTable(); // returns a list of colors contained in the image's color table.
+
+                 for ( QVector<QRgb>::const_iterator it = v.begin(), itE = v.end(); it != itE; ++it )
+                {
+                    QColor clrCurrent( *it );
+                    qDebug()<< "Red: " << clrCurrent.red()<< " Green: " << clrCurrent.green()<< " Blue: " << clrCurrent.blue()<< " Alpha: " << clrCurrent.alpha()<<"\n";
+                    bv+=clrCurrent.blue();
+                    gv+=clrCurrent.green();
+                    rv+=clrCurrent.red();
+                }
+                 QImage *tmp =  new QImage(img);
+                 MyImage *Img= new MyImage(tmp,rv,gv,bv);
+                 mImageList<<Img;
+            }
         }
     }
 }
 
 
-void ImageProcessor::printRGB(CType t)
+void ImageProcessor::showImage(CType t,int ad)
 {
-    QVector<QImage>::Iterator it;
-    QImage tImage;
-    for (it = mImage.begin(); it != mImage.end(); ++it)
-    {
-        tImage = *it;
 
-    }
 
 
 }
--- C_C++/qtapps/konylabs/ImageProcessing/imageprocessor.h
+++ C_C++/qtapps/konylabs/ImageProcessing/imageprocessor.h
@@ -2,6 +2,11 @@
 #define IMAGEPROCESSOR_H
 #include<QImage>
 #include<QVector>
+#include<QDebug>
+#include<QColor>
+
+
+
 
 typedef enum
 {
@@ -10,16 +15,43 @@ typedef enum
     BLUE
 }CType;
 
+class MyImage
+{
+public:
+    MyImage(QImage *img,int rv,int gv,int bv):mImage(img),rValue(rv),gValue(gv),bValue(bv),next(0)
+    {
+        qDebug()<<"MyImage::MyImage\n";
+    }
+    
+    QImage *mImage;
+    int rValue;
+    int gValue;
+    int bValue;
+    MyImage *next;
+};
+
+class MyImageList
+{
+public:
+    MyImageList():mHead(0){}
+    MyImageList & operator <<(MyImage *);
+    void ascending(CType t);
+    void descending(CType t);
+private:
+    MyImage *mHead;
+};
+
+
 class ImageProcessor
 {
 public:
     explicit ImageProcessor(QString &path);
     void process();
-    void printRGB(CType t);
+    void showImage(CType t,int ad=0);
     ~ImageProcessor();
 private:
-    QImage *mSrc;
-    QVector<QImage> mImages;
+   MyImageList mImageList;
+   QImage *mSrc;
 };
 
 #endif // IMAGEPROCESSOR_H
