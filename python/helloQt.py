40d27795e04ab6f363fb0fc363096bc66ec7548f
unknown <oeryuksel@infodif.com>
2017-11-01 22:21:32 +0300

    changes for python qt

--- python/helloQt.py
+++ python/helloQt.py
@@ -1,20 +1,18 @@
 import sys
-import PyQt5
-from PyQt5 import QtCore ,QtGui , QtWidgets
-from PyQt5.QtCore import QDateTime, QThread
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
-def myThread(arg):
-    print (arg)
+if __name__ == '__main__':
+    app = QtWidgets.QApplication.instance()
+    if app is None:
+        app = QtWidgets.QApplication(sys.argv)
+    else:
+        print('QApplication instance already exists: %s' % str(app))
 
-
-if __name__=="__main__":
-    try:
-        datetime = QDateTime()
-        print (datetime.currentDateTime())
-        thread  = QThread(myThread)
-        #thread.
-        thread.start()
-    except BaseException as e:
-        print (str(e))
-        
+    window = QtWidgets.QWidget()
+    window.setGeometry(100, 100, 500, 300)
+    window.setWindowTitle("PyQT Tuts!")
+    window.show()
+    app.exec_()
