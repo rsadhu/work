48c15dd10e13ce5f01743fde7e39847b34045211
rsadhu <rakeshsadhu@gmail.com>
2018-07-13 17:53:32 +0200

    initial commit , has c program 1st array prog, cppadd prog and python script for cleanup from root folder

---
+++ cleanup.py
@@ -0,0 +1,27 @@
+import os
+#def cleanup():
+#cur_dir =  os.getcwd()
+#file_name ="arrays.c"
+#while True:
+#file_list = os.listdir(cur_dir)
+#parent_dir = os.path.dirname(cur_dir)
+#if file_name in file_list:
+#print "exists in ", cur_dir, "  ",file_name
+#break
+#else	/* : */
+#if cur_dir == parent_dir:
+#print "File not found"
+#else:
+#cur_dir = parent_dir
+  
+import os, fnmatch 
+def find (pattern, path):
+    result =[]
+    for root, dirs, files in os.walk (path):
+        for name in files:
+            if fnmatch.fnmatch (name, pattern):
+                result.append (os.path.join (root, name))
+	return result 
+
+
+print find('*.*~' ,'/home/rsadhu/')
