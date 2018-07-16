d3bafbe12387361d85d7dba43b14ff1eb2fdb7a2
rsadhu <rakeshsadhu@gmail.com>
2018-07-16 07:15:38 +0200

    remove CTI  and modified a little cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -18,7 +18,10 @@ import os, fnmatch
 def find (pattern, path):
     result =[]
     for root, dirs, files in os.walk (path):
+        print root  + " parent  "
+     #   print dirs  + "  dirs  "
         for name in files:
+            print name  +" file  "
             if fnmatch.fnmatch (name, pattern):
                 result.append (os.path.join (root, name))
 	return result 
