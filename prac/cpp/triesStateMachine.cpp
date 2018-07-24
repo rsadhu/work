2c715f402759e68989b705a840780c396eceb792
Rakesh Sadhu <rsadhu@luxoft.com>
2018-07-24 23:12:57 +0200

    adding trie

--- prac/cpp/triesStateMachine.cpp
+++ prac/cpp/triesStateMachine.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
+#include<map>
 
-class TrieSM
+using mmap = std::map <char, std::map <char,char>>;
+
+class  TrieSM
 {
-};
-
-int main(void)
+    public:
+        void operator < (char ch); 
+        void show (); 
+    protected:
+        void reset (); 
+        void add ();
+    private:
+        mmap mTrieDatasource;
+}; 
+                    
+                    
+                    
+int main (void)
 {
     return 0;
 }
