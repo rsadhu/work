5a89791b65509dfbee2c5916f06b17fa74ccd546
Rakesh Sadhu <rsadhu@luxoft.com>
2019-01-16 18:21:38 +0100

    qml learn

--- playground/cpp/deprecated_cpp14.cpp
+++ playground/cpp/deprecated_cpp14.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<memory>
 
 struct[[deprecated ("deprecated")]] S;
+
+class Base
+{
+    public:
+    Base(){std::cout<<"Base::Base\n";}
+    ~Base(){ std::cout<<"Base::~Base\n";}
+    int add(int a, int b, int c) { return a+b+c;}
+};
+
+class [[deprecated("Wakka wakka this isn't supposed to be used")]] Test 
+{
+    public:
+    void display()
+    {
+        std::cout<<"sum(1,3,5)"<< mPtr.add(1, 3, 5);
+    }
+    private:
+    std::unique_ptr<Base> mPtr;
+};
 
 
 [[deprecated()]]
@@ -9,20 +29,19 @@ int  add(int b, int a)
  return a+b;
 }
 
-[[deprecated()]] typedef s*t;
+struct sttt
+{
+    int data;
+};
+
+[[deprecated]] typedef sttt s;
 
 int
 main (void)
 {
-
-  struct sttt
-  {
-    int data;
-  };
-
-  typedef struct sttt T;
-   
-  std::cout<<add(1,1);  
-
-  return 0;
+   std::cout<<add(1,1);  
+   Test t;   
+   t.display();
+   s a;
+   return 0;
 }
