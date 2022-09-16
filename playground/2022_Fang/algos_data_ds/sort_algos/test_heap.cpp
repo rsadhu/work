2682fd40ce4729c193938e8105bb6f6ebc717b64
Rakesh Sadhu <rakeshsadhu@gmail.com>
2022-09-16 23:57:52 +0200

    add test heap

---
+++ playground/2022_Fang/algos_data_ds/sort_algos/test_heap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+void heapify(std::vector<int> &rhs, int index)
+{
+}
+
+int main(void)
+{
+    std::vector<int> l = {};
+    heapsort(l);
+    return 0;
+}
