662430eac872e3526719b8e2a24f01191400236d
Rakesh Sadhu <rakeshsadhu@gmail.com>
2022-10-13 15:25:29 +0200

    add dynamic_prog js folder

--- playground/top_probs/count_islands.cpp
+++ playground/top_probs/count_islands.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+
+class Base
+{
+public:
+    Base()
+    {
+        std::cout << "\nBase\n";
+    }
+
+    ~Base()
+    {
+        std::cout << "\n~Base\n";
+    }
+};
+
+class Der : Base
+{
+public:
+    Der()
+    {
+        std::cout << "\nDer\n";
+    }
+
+    ~Der()
+    {
+        std::cout << "\n~Der\n";
+    }
+};
+
+int main(void)
+{
+    Der d;
+    return 0;
+}
 #include <queue>
 #include <stack>
 #include <set>
